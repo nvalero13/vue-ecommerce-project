2713fb40cc064620ec25c3d021d353cbd8b6037f
Nil Valero <nvalero13@gmail.com>
2022-12-05 13:47:07 +0100

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex justify-between items-center bg-stone-400 px-60 py-6">
+    <h2 class="text-3xl text-white font-bold">LOGO</h2>
+    <nav>
+        <ul class="flex justify-between text-white flex-1 gap-20">
+            <li class="cursor-pointer border-stone-400 border-b-2 py-1 hover:border-white transition-all"><a href="">Page 1</a></li>
+            <li class="cursor-pointer border-stone-400 border-b-2 py-1 hover:border-white transition-all"><a href="">Page 2</a></li>
+            <li class="cursor-pointer border-stone-400 border-b-2 py-1 hover:border-white transition-all"><a href="">Page 3</a></li>
+        </ul>
+    </nav>
+    <button class="text-white border-2 rounded px-5 py-2 border-white hover:text-stone-400 hover:bg-white transition-all">Carro</button>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
